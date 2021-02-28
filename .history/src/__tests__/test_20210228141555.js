ee0b7e963e9b23674238e013f433debaf7e37e31
Blessie Suresh <sureshblessie@gmail.com>
2021-02-28 14:34:50 +0100

    Added more test

--- .history/src/__tests__/test_20210228141555.js
+++ .history/src/__tests__/test_20210228141555.js
@@ -2,21 +2,14 @@ import App from '../App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { render } from '@testing-library/react';
-
-describe('App', () => {
-  test('renders App component', () => {
-    render(<App />);
-  });
-});
 
 test('renders the link of the nav bar', () => {
   //render(<BrowserRouter><AppNavbar/></BrowserRouter>);
   // const element = screen.getByText("Github for back");
   //expect(element).toBeInTheDocument();
 
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+
 
 });
 
