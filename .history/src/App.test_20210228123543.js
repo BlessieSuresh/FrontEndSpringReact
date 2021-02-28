16372b2737fd32618e8a56d5e4ab808bed0f5afb
Blessie Suresh <sureshblessie@gmail.com>
2021-02-28 12:38:20 +0100

    Added Dummy Test

--- .history/src/App.test_20210228123543.js
+++ .history/src/App.test_20210228123543.js
@@ -1,19 +1,12 @@
+import { render, screen } from '@testing-library/react';
 import App from './App';
+import AppNavbar from "./AppNavbar";
+import BrowserRouter from "react-router-dom/BrowserRouter";
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import App from './App';
 
 test('renders the link of the nav bar', () => {
-  //render(<BrowserRouter><AppNavbar/></BrowserRouter>);
-  // const element = screen.getByText("Github for back");
-  //expect(element).toBeInTheDocument();
-
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-
-});
-
-test('renders the link of the nav bar2', () => {
   //render(<BrowserRouter><AppNavbar/></BrowserRouter>);
   // const element = screen.getByText("Github for back");
   //expect(element).toBeInTheDocument();
--- .history/src/App.test_20210228123617.js
+++ .history/src/App.test_20210228123617.js
@@ -12,13 +12,3 @@ test('renders the link of the nav bar', () => {
   ReactDOM.render(<App />, div);
 
 });
-
-test('renders the link of the nav bar2', () => {
-  //render(<BrowserRouter><AppNavbar/></BrowserRouter>);
-  // const element = screen.getByText("Github for back");
-  //expect(element).toBeInTheDocument();
-
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-
-});
