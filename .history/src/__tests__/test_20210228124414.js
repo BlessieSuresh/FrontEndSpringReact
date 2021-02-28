2b4095c579577490cab9ec7908c4b8d877df034c
Blessie Suresh <sureshblessie@gmail.com>
2021-02-28 12:45:21 +0100

    Created __tests__ and moved test.js inside it

---
+++ .history/src/__tests__/test_20210228124414.js
@@ -0,0 +1,24 @@
+import App from '../../src/App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+test('renders the link of the nav bar', () => {
+  //render(<BrowserRouter><AppNavbar/></BrowserRouter>);
+  // const element = screen.getByText("Github for back");
+  //expect(element).toBeInTheDocument();
+
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+
+});
+
+test('renders the link of the nav bar2', () => {
+  //render(<BrowserRouter><AppNavbar/></BrowserRouter>);
+  // const element = screen.getByText("Github for back");
+  //expect(element).toBeInTheDocument();
+
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+
+});
